74ca5d8e9bf258a034e1a23d6da47f82bcf36519
Илья <ilja_boroznov@mail.ru>
2022-09-25 19:21:28 +0300

    Добавил задачу 13

---
+++ Home-work-2/ДЗ-задача-013/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+if ( number < 99 ) Console.WriteLine($"Третья цифра цисла {number} отсутствует");
+if ( number < 999 && number > 99 )
+{
+    int thirdDigit = number % 10;
+    Console.WriteLine($"Третья цифра цисла {number} => {thirdDigit}");
+}
+int num = number;
+if ( number > 999 )
+{
+        while( num > 999 )
+        {
+        num = num / 10;
+        }
+    int thirdDigit = num % 10;
+    Console.WriteLine($"Третья цифра цисла {number} => {thirdDigit}");
+}
