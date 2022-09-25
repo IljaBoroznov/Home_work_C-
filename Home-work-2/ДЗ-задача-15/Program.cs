b2b916fc1ae5079d2a2cd465459fe9c627497a00
Илья <ilja_boroznov@mail.ru>
2022-09-25 19:27:09 +0300

    Добавил задачу 15

---
+++ Home-work-2/ДЗ-задача-15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("нет");
+        break;
+    case 2:
+        Console.WriteLine("нет");
+        break;
+    case 3:
+        Console.WriteLine("нет");
+        break;
+    case 4:
+        Console.WriteLine("нет");
+        break;
+    case 5:
+        Console.WriteLine("близко, но нет");
+        break;
+    case 6:
+        Console.WriteLine("ДААА!");
+        break;
+    case 7:
+        Console.WriteLine("ДААА!!");
+        break;
+    default:
+        Console.WriteLine("Неверный номер дня недели");
+        break;
+}
