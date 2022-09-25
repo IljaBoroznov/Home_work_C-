ce858c063276783fdcc82894794151b26d594dbd
Илья <ilja_boroznov@mail.ru>
2022-09-25 17:32:12 +0300

    Добавил решение 10 задачи

---
+++ Home_work_1/ДЗ-задача-10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+int secondDigit;
+secondDigit = number / 10 % 10;
+
+Console.WriteLine($"вторая цифра цисла {number} => {secondDigit}");
