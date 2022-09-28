9fa38e556b2057fbb0d697e152ed37a0f8d7e687
Илья <ilja_boroznov@mail.ru>
2022-09-28 21:04:45 +0300

    add task-23

---
+++ Home-work-3/task-23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N ");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while( count <= n)
+{
+    Console.WriteLine( $" куб числа {count} = {count * count * count} ");
+    count++;
+}
