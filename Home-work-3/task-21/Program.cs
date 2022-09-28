03e44a8b16185bf595758e4b7035dc23d65d4d37
Илья <ilja_boroznov@mail.ru>
2022-09-28 20:45:19 +0300

    correct task-17

---
+++ Home-work-3/task-21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите Х точки А ");
+int ax;
+ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите У точки А  ");
+int ay;
+ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z точки A  ");
+int az;
+az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Х точки В ");
+int bx;
+bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите У точки В  ");
+int by;
+by = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z точки В  ");
+int bz;
+bz = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb)); // квадратный корень из числа
+double resultRound = Math.Round(result, 2); // округление до 2 знаков после запятой
+
+
+Console.WriteLine($" AB = {resultRound}");
