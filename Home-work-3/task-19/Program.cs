9f75707e44f204164b0c75b9edadbafb85e0cb19
Илья <ilja_boroznov@mail.ru>
2022-09-28 20:23:03 +0300

    add task-19

---
+++ Home-work-3/task-19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+if ( number > 99999 || number < 10000) 
+Console.WriteLine("Данное число не пятизначное");
+int firsttwodigit = number / 1000;
+
+while (number > 100)
+{
+    number = number % 100;
+}
+
+int lasttwonumber = number % 10 * 10 + number / 10;
+
+if (firsttwodigit == lasttwonumber) 
+Console.WriteLine("Данное число палиндром");
+else 
+Console.WriteLine("данное число не палиндром");
