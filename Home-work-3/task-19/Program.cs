2d6bc99fda2625a81522f439f5afea87ae542ea0
Илья <ilja_boroznov@mail.ru>
2022-10-02 12:41:54 +0300

    add task-25

--- Home-work-3/task-19/Program.cs
+++ Home-work-3/task-19/Program.cs
@@ -1,19 +1,17 @@
 ﻿Console.WriteLine("Введите пятизначное число ");
 int number;
 number = Convert.ToInt32(Console.ReadLine());
-if ( number > 99999 || number < 10000) 
-Console.WriteLine("Данное число не пятизначное");
-return;
-int firsttwodigit = number / 1000;
+
+int twofirstdigit = number / 1000;
 
 while (number > 100)
 {
     number = number % 100;
 }
 
-int lasttwonumber = number % 10 * 10 + number / 10;
+int twolastnumber = number % 10 * 10 + number / 10;
 
-if (firsttwodigit == lasttwonumber) 
+if (twofirstdigit == twolastnumber) 
 Console.WriteLine("Данное число палиндром");
 else 
 Console.WriteLine("данное число не палиндром");
---
+++ Home-work-4/task-25/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число A ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+
+int DegreeAinB(int A, int B)
+{
+    int length = B;
+    int result = A;
+    for (int i = 1; i < length; i++ )
+        {
+            result = result * A;
+        }
+    return result;
+}
+
+int degree = DegreeAinB(a, b);
+
+Console.WriteLine( $" {b}-я степень числа {a} = {degree} ");
