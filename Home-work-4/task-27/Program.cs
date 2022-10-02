663320ad6100bc8f10495c3e90fb9ccf56291087
Илья <ilja_boroznov@mail.ru>
2022-10-02 13:41:37 +0300

    add task-27

---
+++ Home-work-4/task-27/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите число  ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+
+int SummDigit(int num)
+{
+    int order = num;
+    int i = 1;
+    while( order > 1)
+    {
+        order = order / 10;
+        i++;
+    }
+        int result = default;
+        switch (i)
+        {
+            case 1:
+            Console.Write("сумма равна введенному числу  ");
+            result = num;
+                break;
+            case 2:
+            result = num / 10 + num % 10;
+                break;
+            case 3:
+            result = num / 100 + num / 10 % 10 + num % 10;
+                break;
+            case 4:
+            result = num / 1000 + num / 100 % 10 + num / 10 % 10 + num % 10;
+                break; 
+            case 5:
+            result = num / 10000 + num / 1000 % 10 + num / 100 % 10 + num /10 % 10 + num % 10;
+                break;  
+            case 6:
+            result = num / 100000 + num / 10000 % 10 + num / 1000 % 10 + num /100 % 10 + num / 10 % 10 + num % 10;
+                break; 
+            default:
+            Console.WriteLine("превышен формат");
+                break;
+        }
+return result;
+}
+
+//int summgigit = SummDigit(number);
+Console.WriteLine(SummDigit(number));
