5f63a0891d17196338aa3a57ec3a00db3e0532e1
Илья <ilja_boroznov@mail.ru>
2022-10-02 13:47:05 +0300

    add task-29

---
+++ Home-work-4/task-29/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите длину массива ");
+int length;
+length = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = new int[length];
+FillArray(array1);
+PrintArray(array1);
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random(); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 99);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length -1]}]");
+    Console.WriteLine();
+}
