bf29b167e6a1051c3526a3b461735cb3036941f9
Илья <ilja_boroznov@mail.ru>
2022-10-16 23:01:48 +0300

    correct task-60

--- Home-work-8/task-60/Program.cs
+++ Home-work-8/task-60/Program.cs
@@ -28,18 +28,85 @@ int[,,] matrix = CreateMatrixRndInt(rows, columns, depth, min, max);
 PrintMatrix(matrix);
 
 
+bool SearchDigit(int[,,] matr, int x)
+{
+    int result = default;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int z = 0; z < matr.GetLength(2); z++)
+            {
+                if( x == matr[i, j, z])
+                {
+                    result = 0;
+                }
+                else
+                result = 1;
+
+            }
+        }
+    }
+    return result;
+}
 
 int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
 {
     int[,,] matrix = new int[rows, columns, depth];
     Random rnd = new Random();
+    int check = default;
+    int a = default;
     for (int i = 0; i < matrix.GetLength(0); i++)   //(0) - rows
     {
       for (int j = 0; j < matrix.GetLength(1); j++)  //(1)- columns
         {
-            for (int z = 0; z < matrix.GetLength(2); z++)
+            for (int z = 0; z < matrix.GetLength(2);)
             {
-                matrix[i, j, z] = rnd.Next(min, max + 1);
+                a = rnd.Next(min, max + 1);
+                bool Povtor;
+                Povtor = false;
+                for (int k = 0; k < matrix.GetLength(0); k++)
+                {
+                    for (int n = 0; n < matrix.GetLength(1); n++)
+                    {
+                        for (int m = 0; m < matrix.GetLength(2); m++)
+                        {
+                            
+                            if(matrix[k, n, m] == a)
+                            {
+                                Povtor = true;
+                                break;
+                            }
+                            
+                        }
+                
+                    }
+                }
+            if(!Povtor)
+            {
+            matrix[i, j, z] = a;
+            z++;
+            }
+
+
+
+                //check = SearchDigit(matrix, a);
+                /*while(check == 1)
+                {
+                    a = rnd.Next(min, max + 1);
+                    check = SearchDigit(matrix, a);
+                }
+                matrix[i, j, z] = a;
+                */
+                /*
+                if(check == 1)
+                {
+                    matrix[i, j, z] = a;
+                    z++;
+                }
+                */
+                
+                         
             }
         } 
     } 
